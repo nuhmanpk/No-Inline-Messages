ebb50862313fe53f635d7882a7a684807d492772
Nuhman Pk <62880706+bughunter0@users.noreply.github.com>
2021-09-23 12:09:26 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import os
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+bughunter0 = Client(
+    "BotNameHere",
+    bot_token = os.environ["BOT_TOKEN"],
+    api_id = int(os.environ["API_ID"]),
+    api_hash = os.environ["API_HASH"]
+)
+
+@bughunter0.on_message(filters.via_bot & filters.group)
+async def inline(bot,message):
+     await message.delete()
+	
+
+bughunter0.run()
